b710375d097acdedc180fd546e551f29642e24d2
Mognus <magnusderkleine@gmail.com>
2026-05-20 18:44:17 +0200

    refactor: unwrap admin mutation responses

--- frontend/lib/api-client.ts
+++ frontend/lib/api-client.ts
@@ -1,18 +1,24 @@
 import { mutateFetch } from "@/lib/api/fetcher";
 import type { AdminRecord } from "./api-server";
 
+interface AdminDataResponse {
+    data: AdminRecord;
+}
+
 export async function fetchAdminCreate(apiPath: string, data: AdminRecord): Promise<AdminRecord> {
-    return mutateFetch<AdminRecord>(apiPath, {
+    const response = await mutateFetch<AdminDataResponse>(apiPath, {
         method: "POST",
         body: data,
     });
+    return response.data;
 }
 
 export async function fetchAdminUpdate(apiPath: string, id: string | number, data: AdminRecord): Promise<AdminRecord> {
-    return mutateFetch<AdminRecord>(`${apiPath}/${id}`, {
+    const response = await mutateFetch<AdminDataResponse>(`${apiPath}/${id}`, {
         method: "PUT",
         body: data,
     });
+    return response.data;
 }
 
 export async function fetchAdminDelete(apiPath: string, id: string | number): Promise<void> {
